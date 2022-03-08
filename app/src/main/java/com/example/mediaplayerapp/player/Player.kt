b2fd4377cc4f4a2d10eb10d10f8583e61d4144b0
Edward Burgos <80774061+edwardburgos@users.noreply.github.com>
2022-03-08 13:38:02 -0500

    :lipstick: styles improved

--- app/src/main/java/com/example/mediaplayerapp/player/Player.kt
+++ app/src/main/java/com/example/mediaplayerapp/player/Player.kt
@@ -2,6 +2,7 @@ package com.example.mediaplayerapp.player
 
 import android.media.MediaPlayer
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons.Filled
@@ -98,34 +99,34 @@ fun Player(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     mediaPlayer.let {
-                        IconButton(onClick = { playerManager.prevSong() }) {
-                            Icon(
-                                imageVector = Filled.SkipPrevious,
-                                contentDescription = "Previous song",
-                                tint = MaterialTheme.colors.primary,
-                                modifier = Modifier.size(40.dp)
-                            )
-                        }
+                        Icon(
+                            imageVector = Filled.SkipPrevious,
+                            contentDescription = "Previous song",
+                            tint = MaterialTheme.colors.primary,
+                            modifier = Modifier
+                                .size(40.dp)
+                                .clickable { playerManager.prevSong() }
+                        )
                         Column(
                             modifier = Modifier.padding(horizontal = 16.dp)
                         ) {
-                            IconButton(onClick = { playerManager.playStop() }) {
-                                Icon(
-                                    imageVector = if (mediaPlayerPlayingState) Filled.Pause else Filled.PlayArrow,
-                                    contentDescription = "Play / Stop",
-                                    tint = MaterialTheme.colors.primary,
-                                    modifier = Modifier.size(64.dp)
-                                )
-                            }
-                        }
-                        IconButton(onClick = { playerManager.nextSong() }) {
                             Icon(
-                                imageVector = Filled.SkipNext,
-                                contentDescription = "Next song",
+                                imageVector = if (mediaPlayerPlayingState) Filled.Pause else Filled.PlayArrow,
+                                contentDescription = "Play / Stop",
                                 tint = MaterialTheme.colors.primary,
-                                modifier = Modifier.size(40.dp)
+                                modifier = Modifier
+                                    .size(70.dp)
+                                    .clickable { playerManager.playStop() }
                             )
                         }
+                        Icon(
+                            imageVector = Filled.SkipNext,
+                            contentDescription = "Next song",
+                            tint = MaterialTheme.colors.primary,
+                            modifier = Modifier
+                                .size(40.dp)
+                                .clickable { playerManager.nextSong() }
+                        )
                     }
                 }
             }
